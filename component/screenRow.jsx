db4e589b5c30c047e7bc0521a55f3cabeb2b8735
sun1211 <ngoctam121191@yahoo.com>
2018-08-12 14:11:09 +0700

    Add files via upload

---
+++ component/screenRow.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Screen row component is written as a functional component
+// it receives and displays (in an input field) a props (property) of value from
+// it's parent component
+const ScreenRow = (props) => {
+  return (
+    <div>
+      <input className={'col-xs-12'} type="text" readOnly value={props.value}/>
+    </div>
+  )
+}
+
+// we describe the props (property) that the parent element is required to pass
+// into this component
+// ScreenRow.propTypes = {
+//   MyClassName: React.PropTypes.string
+// }
+
+export default ScreenRow;
